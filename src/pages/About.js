b8351fa886c3d8934b689348a4d40fe6f655787d
Alex <forcablaugrana@gmail.com>
2022-09-02 13:20:12 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const About = () => (
+		<div className='jumbotron'>
+			<div className='container'>
+				<h1 className='display-4'>React-приложение ToDo</h1>
+				<p className='lead'>Версия приложения <strong>1.0.2</strong></p>
+			</div>
+		</div>
+	)
